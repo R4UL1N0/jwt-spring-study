fe7db89539d580a7268c0be6ae71676ec17c5ce7
R4UL1N0 <raulino.storage@gmail.com>
2023-09-13 19:50:30 -0300

    Implementing passwordEncoder() and authenticationManager() in AppConfig

--- src/main/java/com/raulino/jwtstudyspring/config/AppConfig.java
+++ src/main/java/com/raulino/jwtstudyspring/config/AppConfig.java
@@ -2,7 +2,13 @@ package com.raulino.jwtstudyspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.raulino.jwtstudyspring.services.LocalUserDetailsService;
 
@@ -12,10 +18,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AppConfig {
 
-    
-    
     @Bean
     public UserDetailsService userDetailsService() {
         return new LocalUserDetailsService();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
